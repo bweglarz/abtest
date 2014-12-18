e93111c24f1694941d111d16036e30c0443b8c35
bweglarz <bweglarz@gmail.com>
2014-12-18 10:34:23 +0100

    Fat jar and app test placeholder

---
+++ src/test/java/pl/allegro/abtest/ABTestApplicationTest.java
@@ -0,0 +1,14 @@
+package pl.allegro.abtest;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class ABTestApplicationTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
